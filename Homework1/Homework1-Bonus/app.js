1d4154f12809bf4c5d62a9043c14856ce34c999d
Biohazardx44 <nikolailievski420@gmail.com>
2023-02-20 16:22:39 +0100

    JS Exercises

---
+++ Homework1/Homework1-Bonus/app.js
@@ -0,0 +1,21 @@
+var MPGTolPer100K = 0.00425144;
+
+var lp100kToMPG = 235.21442146661;
+
+var mpg50 = 50;
+
+var mpg25 = 25;
+
+var lp100k = 12;
+
+var mpg50Resoult = MPGTolPer100K * mpg50;
+
+var mpg25Resoult = MPGTolPer100K * mpg25;
+
+var lp100kToMPGResoult = lp100kToMPG * lp100k;
+
+console.log("MPG 50 = ", mpg50Resoult, "l/100k");
+
+console.log("MPG 25 =", mpg25Resoult, "l/100k");
+
+console.log("l/100k =", lp100kToMPGResoult, "MPG");
---
+++ Homework1/script.js
@@ -0,0 +1,4 @@
+var phonePrice = 119.95;
+var tax = (phonePrice / 100) * 5;
+var numberOfPhones = 30;
+console.log('The price of 30 Phones is', numberOfPhones * phonePrice, "$ and with tax deducted it's", numberOfPhones * phonePrice - numberOfPhones * tax, '$.');
